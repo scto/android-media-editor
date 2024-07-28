efcd18fda9b3c59e585576e201bf2218c05f75ca
Matthieu <24-artectrex@users.noreply.shinice.net>
2024-07-28 22:59:31 +0200

    Add note about package upload script

--- android-release.sh
+++ android-release.sh
@@ -1,28 +1,29 @@
 
 # First argument is the Gitlab API token, second argument is the version (tag name)
+# Careful, you need to change the version number manually in the script before running it for now, TODO fix that!
 
 idArm64=$(curl --header "PRIVATE-TOKEN: $1" --upload-file app/release/app-arm64-v8a-release.apk "https://gitlab.shinice.net/api/v4/projects/109/packages/generic/eu.artectrex.eu/$2/app-release.apk?select=package_file" | jq -r .id)
 
 curl --request POST --header "PRIVATE-TOKEN: $1" --data name="APK (arm64-v8a)" \
   --data url="https://gitlab.shinice.net/pixeldroid/bunny/-/package_files/$idArm64/download" \
-  --data direct_asset_path="/package/bunny-$2-102.apk" "https://gitlab.shinice.net/api/v4/projects/109/releases/$2/assets/links"
+  --data direct_asset_path="/package/bunny-$2-202.apk" "https://gitlab.shinice.net/api/v4/projects/109/releases/$2/assets/links"
 
 idArm=$(curl --header "PRIVATE-TOKEN: $1" --upload-file app/release/app-armeabi-v7a-release.apk "https://gitlab.shinice.net/api/v4/projects/109/packages/generic/eu.artectrex.eu/$2/app-release.apk?select=package_file" | jq -r .id)
 
 curl --request POST --header "PRIVATE-TOKEN: $1" --data name="APK (arm)" \
   --data url="https://gitlab.shinice.net/pixeldroid/bunny/-/package_files/$idArm/download" \
-  --data direct_asset_path="/package/bunny-$2-101.apk" "https://gitlab.shinice.net/api/v4/projects/109/releases/$2/assets/links"
+  --data direct_asset_path="/package/bunny-$2-201.apk" "https://gitlab.shinice.net/api/v4/projects/109/releases/$2/assets/links"
 
 idx86=$(curl --header "PRIVATE-TOKEN: $1" --upload-file app/release/app-x86-release.apk "https://gitlab.shinice.net/api/v4/projects/109/packages/generic/eu.artectrex.eu/$2/app-release.apk?select=package_file" | jq -r .id)
 
 curl --request POST --header "PRIVATE-TOKEN: $1" --data name="APK (x86)" \
   --data url="https://gitlab.shinice.net/pixeldroid/bunny/-/package_files/$idx86/download" \
-  --data direct_asset_path="/package/bunny-$2-103.apk" "https://gitlab.shinice.net/api/v4/projects/109/releases/$2/assets/links"
+  --data direct_asset_path="/package/bunny-$2-203.apk" "https://gitlab.shinice.net/api/v4/projects/109/releases/$2/assets/links"
 
 idx8664=$(curl --header "PRIVATE-TOKEN: $1" --upload-file app/release/app-x86_64-release.apk "https://gitlab.shinice.net/api/v4/projects/109/packages/generic/eu.artectrex.eu/$2/app-release.apk?select=package_file" | jq -r .id)
 
 curl --request POST --header "PRIVATE-TOKEN: $1" --data name="APK (x86_64)" \
   --data url="https://gitlab.shinice.net/pixeldroid/bunny/-/package_files/$idx8664/download" \
-  --data direct_asset_path="/package/bunny-$2-104.apk" "https://gitlab.shinice.net/api/v4/projects/109/releases/$2/assets/links"
+  --data direct_asset_path="/package/bunny-$2-204.apk" "https://gitlab.shinice.net/api/v4/projects/109/releases/$2/assets/links"
 
 
