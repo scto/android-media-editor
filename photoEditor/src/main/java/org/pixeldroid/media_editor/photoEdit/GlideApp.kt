09149dd557459138084e91a590cb61d6ee79cc4e
Matthieu <24-artectrex@users.noreply.shinice.net>
2024-10-19 15:23:49 +0200

    Remove GlideApp (fix PixelDroid build failure) and update dependencies

--- photoEditor/src/main/java/org/pixeldroid/media_editor/photoEdit/GlideApp.kt
+++
@@ -1,7 +0,0 @@
-package org.pixeldroid.media_editor.photoEdit
-
-import com.bumptech.glide.annotation.GlideModule
-import com.bumptech.glide.module.AppGlideModule
-
-@GlideModule
-class GlideApp : AppGlideModule()
